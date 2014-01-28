4375ccc0158f2d088f7bbfa1b171ae126cf3bce4
Jean-Philippe Ouellet <jpo@vt.edu>
2014-01-28 00:27:17 -0500

    routing for static

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express')
 
 server.listen(process.env.PORT);
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + 'index.html');
 });
