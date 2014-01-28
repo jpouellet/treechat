ecbd42ee27495b83bd7bb5dbb8fcc03d49291e75
Jean-Philippe Ouellet <jpo@vt.edu>
2014-01-27 23:37:40 -0500

    initial ui mockup, should redo with flexbox

---
+++ server.js
@@ -0,0 +1,15 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(process.env.PORT);
+
+app.use(express.static('static'));
+app.get('/'), function(req, res) {
+	res.sendfile(__dirname + 'index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('disconnect', function() {
+	});
+});
